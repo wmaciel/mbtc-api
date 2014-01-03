483f28796632785bb65f75ef65ff136fec31b18d
Walther Maciel <walthermaciel@gmail.com>
2014-01-03 05:43:34 -0200

    Documentation for common functions

--- mbtcapi/common.py
+++ mbtcapi/common.py
@@ -14,6 +14,13 @@ orderTypes = ["buy", "sell"]
 
 
 def convert(input):
+    """Convert unicode strings into utf-8 inside lists and dicts
+
+    This function was created by stack overflow user Mark Amery
+    (http://stackoverflow.com/users/1709587/mark-amery) and published on the
+    thread http://goo.gl/sNy4xP
+
+    """
     if isinstance(input, dict):
         return {convert(key): convert(value) for key, value in input.iteritems()}
     elif isinstance(input, list):
@@ -25,10 +32,17 @@ def convert(input):
 
 
 def getTonce():
+    """Return a tonce to be used in the signature for authentication
+
+    More information about tonces in
+    https://en.bitcoin.it/wiki/MtGox/API/HTTP#tonce_and_nonce
+
+    """
     return str(int(time.time()))
 
 
 def createSignature(apiCode, method, pin, tonce):
+    """Generate the required authentication signature to be placed on header"""
     H = hmac.new(apiCode, digestmod=hashlib.sha512)
     H.update(method + ':' + pin + ':' + tonce)
     signature = H.hexdigest()
@@ -36,6 +50,7 @@ def createSignature(apiCode, method, pin, tonce):
 
 
 def createHeader(apiKey, signature):
+    """Generate the header to be placed on the POST request"""
     return {
         "headers-type": "application/x-www-form-urlencoded",
         "Key": apiKey,
@@ -44,6 +59,7 @@ def createHeader(apiKey, signature):
 
 
 def parseResponse(response):
+    """Parse the response dict removing unwanted information"""
     response = convert(response)
     if response['success'] == 0:
         return response['error']
