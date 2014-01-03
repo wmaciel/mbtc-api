a0e85082e6ae067e1366cdf05791ed7ece489e33
Walther Maciel <walthermaciel@gmail.com>
2014-01-02 22:54:52 -0200

    Correct the doctrings according to the conventions in http://www.python.org/dev/peps/pep-0257/

--- mbtcapi/public.py
+++ mbtcapi/public.py
@@ -4,9 +4,12 @@ import json
 import common
 
 
-def getTicker(coin, connection=None):
-    """
-    Returns a unicode dict of numbers in the following arrangement:
+def getTicker(coin):
+    """ Return the ticker information of the current state of the exchange.
+
+    The ticker contains a summary of the curent state of the exchange for a
+    given coin.
+    This information is given  as a dict in the following arrangement:
     {
       "ticker": {
         "high": Highest traded price (BRL) today,
@@ -18,11 +21,14 @@ def getTicker(coin, connection=None):
         "date": timestamp of the last ticker update
       }
     }
+
+    Arguments:
+    coin -- "btc" or "ltc", defines which coin the info is about
+
     """
     headers = {"Content-type": "application/x-www-form-urlencoded"}
 
-    if connection is None:
-        connection = httplib.HTTPSConnection(common.mbDomain)
+    connection = httplib.HTTPSConnection(common.mbDomain)
 
     address = "/api/ticker/"
     if coin == 'ltc':
@@ -34,19 +40,24 @@ def getTicker(coin, connection=None):
     return common.convert(output)
 
 
-def getOrderBook(coin, connection=None):
-    """
-    Returns a unicode dict of lists of lists in the following arrangement
+def getOrderBook(coin):
+    """Return the active orders for the given coin
+
+    The orders are given as a dict of lists of lists in the following
+    arrangement
     {
       "asks": list of the selling offers available.
       "bids": list of the buying offers available.
     }
     Where each offer is a list of two elements [price per unit, amount]
+
+    Arguments:
+    coin -- "btc" or "ltc", defines which coin the info is about
+
     """
     headers = {"Content-type": "application/x-www-form-urlencoded"}
 
-    if connection is None:
-        connection = httplib.HTTPSConnection(common.mbDomain)
+    connection = httplib.HTTPSConnection(common.mbDomain)
 
     address = "/api/orderbook/"
     if coin == 'ltc':
@@ -58,9 +69,11 @@ def getOrderBook(coin, connection=None):
     return common.convert(output)
 
 
-def getTrades(coin, timeBegin=None, timeEnd=None, connection=None):
-    """
-    Returns a unicode list of dicts in the following arrangement
+def getTrades(coin, timeBegin=None, timeEnd=None):
+    """ Return the history of trades of a given coin in a period of time
+
+    The history of the transactions is given as a list of dicts in the
+    following arrangement:
     [
       {
         "date": Timestamp of the transaction,
@@ -69,12 +82,17 @@ def getTrades(coin, timeBegin=None, timeEnd=None, connection=None):
         "tid": Transaction ID,
         "type": 'buy' or 'sell'
       }
-     ]
+    ]
+
+    Arguments:
+    coin -- "btc" or "ltc", defines which coin the info is about
+    timeBegin -- (optional) Timestamp of the begining of the wanted history
+    timeEnd -- (optional) Timestamp of the end of the wanted history
+
     """
     headers = {"Content-type": "application/x-www-form-urlencoded"}
 
-    if connection is None:
-        connection = httplib.HTTPSConnection(common.mbDomain)
+    connection = httplib.HTTPSConnection(common.mbDomain)
 
     address = "/api/trades/"
     if coin == 'ltc':
